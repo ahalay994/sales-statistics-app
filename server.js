18df32a4b3601c7cc0df1211a55d32f092617649
Nikolay Gudyno <gudyno@lbr.ru>
2022-07-09 19:14:18 +0300

    Fix access to heroku

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const app = express();
+const server = express();
 const path = require('path');
 const port = process.env.PORT || 5000;
-app.use(express.static('dist'));
-app.get('*', (req, res) => {
+server.use(express.static('dist'));
+server.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
 });
 
-app.listen(port, () =>
+server.listen(port, () =>
     console.log(`Server is running on: http://localhost:${port}`)
 );
